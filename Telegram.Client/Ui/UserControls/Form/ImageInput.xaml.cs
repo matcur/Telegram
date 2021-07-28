6b0eb5b4e5d9f6d042645aba573d8b383ef3d155
matsur <ru.far2014@gmail.com>
2021-07-28 18:24:42 +0500

    new: добавил отобрадение выбранного изображения в ImageInput

--- Telegram.Client/Ui/UserControls/Form/ImageInput.xaml.cs
+++ Telegram.Client/Ui/UserControls/Form/ImageInput.xaml.cs
@@ -1,12 +1,19 @@
 using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using UserControl = System.Windows.Controls.UserControl;
 
 namespace Telegram.Client.Ui.UserControls.Form
 {
-    public partial class ImageInput : UserControl, IVisualInput
+    public partial class ImageInput : UserControl, IVisualInput, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged = delegate{  };
+
         public event Action<string> ValueChanged = delegate{  };
 
         public string Error { get; set; }
@@ -18,14 +25,44 @@ namespace Telegram.Client.Ui.UserControls.Form
             {
                 this.value = value;
                 ValueChanged(value);
+                Source = Bitmap(new Uri(value, UriKind.Absolute));
             }
         }
-        
+
+        public ImageSource Source
+        {
+            get => source;
+            set
+            {
+                source = value;
+                OnPropertyChanged();
+            }
+        }
+
         private string value;
+        
+        private ImageSource source;
+
+        private ImageSource preview;
 
         public ImageInput()
         {
+            preview = Bitmap(new Uri("pack://application:,,,/Ui/Resources/Images/new-chat-img.png"));
+            Source = preview;
+            DataContext = this;
+            
             InitializeComponent();
+        }
+
+        private BitmapImage Bitmap(Uri uri)
+        {
+            var result = new BitmapImage();
+            
+            result.BeginInit();
+            result.UriSource = uri;
+            result.EndInit();
+
+            return result;
         }
 
         private void OnClick(object sender, RoutedEventArgs e)
@@ -39,6 +76,11 @@ namespace Telegram.Client.Ui.UserControls.Form
             {
                 Value = dialog.FileName;
             }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
