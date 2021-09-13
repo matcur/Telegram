b1bf56940df1e0627c8e65f8986a780a9bd32787
matsur <ru.far2014@gmail.com>
2021-09-13 17:14:23 +0500

    new: add Custom.AspNet.Filesystem project

---
+++ Custom.AspNet.Filesystem/Files/ApplicationFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+
+namespace Custom.AspNet.Filesystem.Files
+{
+    public class ApplicationFile : IAspFile
+    {
+        public static readonly string Root = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+        
+        private readonly IAspFile _source;
+
+        public ApplicationFile(IFormFile source, string folder)
+            : this(new RandomFile(source, Path.Combine(Root, folder)))
+        {
+
+        }
+
+        public ApplicationFile(IAspFile source)
+        {
+            _source = source;
+        }
+        
+        public string Save()
+        {
+            var path = _source.Save();
+            var parts = path.Split($"{Root}\\");
+            if (parts.Length == 1)
+            {
+                throw new Exception("File path doesn't contains '\\wwwroot'.");
+            }
+
+            var publicPath = parts[1];
+
+            return publicPath;
+        }
+    }
+}
---
+++ Custom.AspNet.Filesystem/IAspFiles.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Custom.AspNet.Filesystem
+{
+    public interface IAspFiles
+    {
+        IEnumerable<string> Save();
+    }
+}
