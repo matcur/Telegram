374fb5bdc254edcdaad77779c9cbac0d5352889e
matsur <ru.far2014@gmail.com>
2021-09-23 15:42:39 +0500

    new: add api on client for chat creation

--- Telegram.Server/ClientApp/src/api/ChatsApi.ts
+++ Telegram.Server/ClientApp/src/api/ChatsApi.ts
@@ -6,13 +6,6 @@ export class ChatsApi {
   api = new ApiClient()
 
   async add(chat: Omit<Chat, 'id' | 'messages'>) {
-    let id = Math.round(Math.random() * 1000);
-
-    return new Promise(res => res(1))
-      .then(res => ({
-        ...chat,
-        id,
-        messages: [],
-      }))
+    return this.api.post<Chat>('chats/create', chat)
   }
 }
