18046b6165146c610ed753a4c2409495b2b0b024
matsur <ru.far2014@gmail.com>
2021-09-25 17:03:48 +0500

    new: add nested properties to FormData presentation

--- Telegram.Server/ClientApp/src/utils/toFormData.ts
+++ Telegram.Server/ClientApp/src/utils/toFormData.ts
@@ -1,4 +1,4 @@
-export const toFormData = (data: any, form: FormData | null = null) => {
+export const toFormData = (data: any, form: FormData | null = null, namespace: string = '') => {
   let formData = form || new FormData()
   for (const key in data) {
     const value = data[key]
@@ -6,26 +6,32 @@ export const toFormData = (data: any, form: FormData | null = null) => {
       continue
     }
 
-    seed(value, formData, key);
+    seed(value, formData, key, namespace)
   }
 
   return formData
 }
 
-function seed(value: any, formData: FormData, key: string) {
+function seed(value: any, formData: FormData, key: string, namespace: string) {
+  const formKey = namespace? `${namespace}[${key}]`: key
+
   if (value instanceof Date) {
-    formData.append(key, value.toDateString())
+    formData.append(formKey, value.toDateString())
   } else if (value instanceof Array) {
-    value.forEach(element => {
-      if (typeof element != 'object')
-        formData.append(`${key}[]`, element)
+    value.forEach((element, index) => {
+      if (typeof element != 'object') {
+        console.log(`${formKey}[]`, element)
+        formData.append(`${formKey}[]`, element)
+      }
       else {
-        toFormData(element, formData)
+        const multiDimensionKey = `${formKey}[${index}]`
+        toFormData(element, formData, multiDimensionKey)
       }
     })
   } else if (typeof value === 'object' && !(value instanceof File)) {
-    toFormData(value, formData)
+    toFormData(value, formData, formKey)
   } else {
-    formData.append(key, value.toString())
+    console.log(formKey, value.toString())
+    formData.append(formKey, value.toString())
   }
 }
