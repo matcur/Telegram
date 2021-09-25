59817f96ef4578a4758b4d60a3d0630fa99c368e
matsur <ru.far2014@gmail.com>
2021-09-25 14:42:41 +0500

    new: add current user info

--- Telegram.Server/ClientApp/src/components/menus/left-menu/LeftMenuUserInfo.tsx
+++ Telegram.Server/ClientApp/src/components/menus/left-menu/LeftMenuUserInfo.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import cat from "public/images/index/cat-3.jpg";
 import {ReactComponent as Caret} from "public/svgs/caret.svg";
+import {useAppSelector} from "../../../app/hooks";
 
 export const LeftMenuUserInfo = () => {
+  const currentUser = useAppSelector(state => state.authorization.currentUser)
+
   return (
     <div className="left-menu-user-info">
       <div className="df jcsb aic top-part">
-        <img className="circle middle-avatar" src={cat} alt="image"/>
+        <img className="circle middle-avatar" src={cat} alt="user-avatar"/>
         <div className="circle saved-messages">
           <Caret/>
         </div>
       </div>
       <div className="df jcsb aic bottom-part">
         <div className="user-details">
-          <div className="user-name">Resurect Resurectovic</div>
-          <div className="phone-number">+7 944 123 22 22</div>
+          <div className="user-name">{currentUser.firstName} {currentUser.lastName}</div>
+          <div className="phone-number">{currentUser.phone?.number}</div>
         </div>
       </div>
     </div>
