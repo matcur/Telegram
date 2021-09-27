bc7ff1a8803e49d7920ef048496cad9e1b93237c
matsur <ru.far2014@gmail.com>
2021-09-27 17:58:36 +0500

    ref: remove solution paths

--- Telegram.Server/ClientApp/src/routes.tsx
+++ Telegram.Server/ClientApp/src/routes.tsx
@@ -2,8 +2,8 @@ import { Route } from 'react-router-dom';
 import {Start} from "pages/Start";
 import {Login} from "pages/Login";
 import {Index} from "pages/Index";
-import {PhoneVerification} from "Telegram.Server/ClientApp/src/pages/PhoneVerification";
-import {TelegramVerification} from "Telegram.Server/ClientApp/src/pages/TelegramVerification";
+import {PhoneVerification} from "pages/PhoneVerification";
+import {TelegramVerification} from "pages/TelegramVerification";
 import React from "react";
 
 export const routes = [
