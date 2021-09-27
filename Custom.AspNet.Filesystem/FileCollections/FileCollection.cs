147a8825bd09b6816e7280fc05172436ef9ae93a
matsur <ru.far2014@gmail.com>
2021-09-27 15:38:26 +0500

    ref: rename ApplicationFiles to FileCollection

--- Custom.AspNet.Filesystem/FileCollections/FileCollection.cs
+++ Custom.AspNet.Filesystem/FileCollections/FileCollection.cs
@@ -5,19 +5,19 @@ using Microsoft.AspNetCore.Http;
 
 namespace Custom.AspNet.Filesystem.FileCollections
 {
-    public class ApplicationFiles : IAspFiles
+    public class FileCollection : IAspFiles
     {
         private readonly IEnumerable<IFormFile> _files;
         
         private readonly Func<IFormFile, string> _fileCreation;
 
-        public ApplicationFiles(IEnumerable<IFormFile> files, string folder)
+        public FileCollection(IEnumerable<IFormFile> files, string folder)
             : this(files, file => new ApplicationFile(file, folder).Save())
         {
 
         }
 
-        public ApplicationFiles(IEnumerable<IFormFile> files, Func<IFormFile, string> fileCreation)
+        public FileCollection(IEnumerable<IFormFile> files, Func<IFormFile, string> fileCreation)
         {
             _files = files;
             _fileCreation = fileCreation;
