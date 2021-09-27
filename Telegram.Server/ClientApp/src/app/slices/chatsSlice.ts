1835efe35bb4e5d982ac1cf985cb1430448710d5
matsur <ru.far2014@gmail.com>
2021-09-27 18:33:55 +0500

    ref: remove initial chatsSlice state

--- Telegram.Server/ClientApp/src/app/slices/chatsSlice.ts
+++ Telegram.Server/ClientApp/src/app/slices/chatsSlice.ts
@@ -9,29 +9,13 @@ type State = {
   list: Chat[]
 }
 
-const baseMessage: Omit<Message, 'content'> = {
-  id: 1,
-  author: {id: 1, firstName: 'Jon', lastName: 'Yon', chats: [], avatarUrl: ''},
-  chatId: 1,
-  creationDate: 'f',
-}
-let mess1 = {...baseMessage, content: [{type: 'Text', value: 'Some text', displayOrder: 1000}]};
-let mess2 = {...baseMessage, content: [{type: 'Text', value: 'Some text 2', displayOrder: 1000}]};
-let mess3 = {...baseMessage, content: [{type: 'Text', value: 'Some text 3', displayOrder: 1000}]};
-export const initialState = {list: [
-    {id: 1, name: 'React', messages: [mess2, {...mess1, author: {id: 4, firstName: 'Jon 2', lastName: 'Yon 2', chats: [], avatarUrl: ''}}, mess1, mess2], members: []},
-    {id: 2, name: 'Typescript', messages: [mess2, mess3], members: []},
-    {id: 3, name: 'Vue', messages: [mess3, mess1, mess2, mess1, mess2, mess1, mess2, mess1, mess2, mess1, mess2, mess1, mess2], members: []},
-  ]} as State
-
 const newMessage = (message: Omit<Message, 'id' | 'creationDate'>, messages: Message[]) => {
   return {...message, id: maxId(messages), creationDate: (new Date()).toDateString()}
 }
 
-// Todo ref this shit
 const chatsSlice = createSlice({
   name: 'chats',
-  initialState,
+  initialState: {list: []} as State,
   reducers: {
     addMessage(state, {payload}: PayloadAction<{chatId: number, message: Omit<Message, 'id' | 'creationDate'>}>) {
       const chat = state.list.find(c => c.id === payload.chatId)
