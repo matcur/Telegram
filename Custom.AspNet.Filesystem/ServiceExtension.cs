ea0b60230be0dff67c9a2a637be9b39d26651fe3
matsur <ru.far2014@gmail.com>
2021-09-27 14:20:47 +0500

    new: add root application folder resolving

---
+++ Custom.AspNet.Filesystem/ServiceExtension.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using Custom.AspNet.Filesystem.Files;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Custom.AspNet.Filesystem
+{
+    public static class ServiceExtension
+    {
+        public static void AddFileSystem(this IServiceCollection services, string wwwrootPath)
+        {
+            ApplicationFile.Root = Path.GetFullPath(
+                Path.Combine(wwwrootPath, @"..\")
+            );
+        }
+    }
+}
