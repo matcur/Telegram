6b038925ef89f260ca4cb39bdff19f9933b83e7d
matsur <ru.far2014@gmail.com>
2021-09-28 15:56:52 +0500

    ref: change message creation resource

--- Telegram.Server/ClientApp/src/api/ChatApi.ts
+++ Telegram.Server/ClientApp/src/api/ChatApi.ts
@@ -11,14 +11,14 @@ export class ChatApi {
   }
   
   async messages(offset: number, count: number) {
-    return await this.api.get<Message[]>(
+    return await this.api.get<{success: boolean, result: Message[]}>(
       `chats/${this.id}/messages?offset=${offset}&count=${count}`
     )
   }
 
   async addMessage(data: FormData) {
-    return await this.api.post(
-      `chats/${this.id}/messages/create`,
+    return await this.api.post<Message>(
+      `messages/create`,
       data
     )
   }
