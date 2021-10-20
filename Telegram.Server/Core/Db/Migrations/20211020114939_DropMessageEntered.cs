3d0f6ef2443765b39f17ae0e563e2d8371b354d3
matsur <ru.far2014@gmail.com>
2021-10-20 16:50:22 +0500

    new: add drop Code.Entered

---
+++ Telegram.Server/Core/Db/Migrations/20211020114939_DropMessageEntered.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Telegram.Server.Core.Db.Migrations
+{
+    public partial class DropMessageEntered : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Entered",
+                table: "Codes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Entered",
+                table: "Codes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
