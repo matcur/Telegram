0f2c7fcfc66635d86b3a2df0271de1af3c4e45c0
matsur <ru.far2014@gmail.com>
2021-10-23 14:57:55 +0500

    new: add header to request in ApiClient.get

--- Telegram.Server/ClientApp/src/api/AuthorizedUserApi.ts
+++ Telegram.Server/ClientApp/src/api/AuthorizedUserApi.ts
@@ -2,16 +2,19 @@ import {ApiClient} from "api/ApiClient";
 import {Chat} from "models";
 
 export class AuthorizedUserApi {
-  api = new ApiClient()
+  readonly api: ApiClient
 
-  constructor(private token: string) {
+  constructor(token: string) {
+    this.api = new ApiClient('1.0', {
+      Authorization: `Bearer ${token}`
+    })
   }
 
   async chats(): Promise<{success: boolean, result: Chat[]}> {
-    return await this.api.get('authorized-user/chats', {
-      headers: {
-        Authorization: `Bearer ${this.token}`
-      }
-    })
+    return await this.api.get('authorized-user/chats')
+  }
+  
+  async changeAvatar(uri: string) {
+    return await this.api.post(`authorized-user/avatar?uri=${uri}`)
   }
 }
