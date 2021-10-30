009e263e8fcf947bc259116fe320a9f5bcc6ed4e
matsur <ru.far2014@gmail.com>
2021-10-30 14:02:01 +0500

    ref: extract RelativePath method

--- Sherden.AspNet.Filesystem/Files/RandomFile.cs
+++ Sherden.AspNet.Filesystem/Files/RandomFile.cs
@@ -27,24 +27,8 @@ namespace Sherden.AspNet.Filesystem.Files
         
         public string Save()
         {
-            string path;
-            var @try = 0;
-            do
-            {
-                var name = _fileName.Value(_source);
-                path = Path.Combine(_folder, name);
-
-                @try++;
-                if (@try > 50)
-                {
-                    throw new Exception(
-                        $"Can't get free file name. Probably {_fileName.GetType()} always returns the same value."
-                    );
-                }
-            } while (File.Exists(path));
-
             return new SimpleFile(
-                _source, path
+                _source, RelativePath(_folder)
             ).Save();
         }
 
@@ -55,6 +39,27 @@ namespace Sherden.AspNet.Filesystem.Files
             new SimpleFile(
                 _source, Path.Combine(_folder, name)
             ).Remove();
+        }
+
+        private string RelativePath(string folder)
+        {
+            string path;
+            var @try = 0;
+            do
+            {
+                var name = _fileName.Value(_source);
+                path = Path.Combine(folder, name);
+
+                @try++;
+                if (@try > 50)
+                {
+                    throw new Exception(
+                        $"Can't get free file name. Probably {_fileName.GetType()} always returns the same value."
+                    );
+                }
+            } while (File.Exists(path));
+
+            return path;
         }
     }
 }
