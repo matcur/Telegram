e64b36baa0affd75529ca70e72fa5db99c3d1060
farhullin.rn <farhullin.rn@greendatasoft.ru>
2022-01-04 16:50:14 +0500

    upd: change spa root folder

--- Telegram.Server/Startup.cs
+++ Telegram.Server/Startup.cs
@@ -84,7 +84,7 @@ namespace Telegram.Server
             services.AddTransient<UserIdentity>();
             
             services.AddSpaStaticFiles(
-                configuration => configuration.RootPath = "ClientApp/build"
+                configuration => configuration.RootPath = "wwwroot"
             );
         }
 
