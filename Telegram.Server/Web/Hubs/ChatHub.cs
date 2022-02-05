7a7ceec4d11e0e53b05e818a7f30518ab903b76f
Farxullin Rustam <34961288+matcur@users.noreply.github.com>
2022-02-06 00:18:29 +0500

    Update ChatHub.cs

--- Telegram.Server/Web/Hubs/ChatHub.cs
+++ Telegram.Server/Web/Hubs/ChatHub.cs
@@ -5,9 +5,19 @@ namespace Telegram.Server.Web.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task EmitMessage(int chatId, string message)
+        public override Task OnConnectedAsync()
         {
-            await Clients.Others.SendAsync("ReceiveMessage", chatId, message);
+            return Groups.AddAsync(Context.ConnectionId, ChatId());
+        }
+
+        public async Task EmitMessage(string message)
+        {
+            await Clients.Group(ChatId).SendAsync("ReceiveMessage", message);
+        }
+
+        private string ChatId()
+        {
+            return Context.Connection.GetHttpContext().Request.Query["chatId"];
         }
     }
 }
