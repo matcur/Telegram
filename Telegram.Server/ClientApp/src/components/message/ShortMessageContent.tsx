12a2c698e98922c0dae47a5d88a2ce5e5c54b527
matcur <ru.far2014@yandex.ru>
2022-02-26 19:35:40 +0500

    new: add image icon to ShotMessageContent

--- Telegram.Server/ClientApp/src/components/message/ShortMessageContent.tsx
+++ Telegram.Server/ClientApp/src/components/message/ShortMessageContent.tsx
@@ -1,5 +1,7 @@
 import React, {FC} from 'react'
 import {Content} from "models";
+import imageIcon from "public/images/image-icon.png"
+import leftPart from "../../public/images/start/start-left-part.png";
 
 type Props = {
   content: Content[]
@@ -7,8 +9,12 @@ type Props = {
 
 export const ShortMessageContent: FC<Props> = ({content}: Props) => {
   const text = content.find(c => c.type === 'Text')
+  const hasImage = content.find(c => c.type === 'Image')
 
   return (
-    <span className="last-message-content">{text?.value?? ''}</span>
+    <span className="last-message-content">
+      {hasImage && <img src={imageIcon} alt=""/>}
+      <span className="last-message-content__text">{text?.value?? ''}</span>
+    </span>
   )
 }
