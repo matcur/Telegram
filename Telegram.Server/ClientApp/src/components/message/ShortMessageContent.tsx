21b4cdd927108d2bcc4c30cfdf29c8e496655096
matcur <ru.far2014@yandex.ru>
2022-02-26 19:36:37 +0500

    ref: remove useless import

--- Telegram.Server/ClientApp/src/components/message/ShortMessageContent.tsx
+++ Telegram.Server/ClientApp/src/components/message/ShortMessageContent.tsx
@@ -1,7 +1,6 @@
 import React, {FC} from 'react'
 import {Content} from "models";
 import imageIcon from "public/images/image-icon.png"
-import leftPart from "../../public/images/start/start-left-part.png";
 
 type Props = {
   content: Content[]
