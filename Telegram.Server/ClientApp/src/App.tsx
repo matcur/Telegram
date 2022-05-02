13fe94ab943b72ca24390d865f5ea400e5cdea43
matcur <ru.far2014@yandex.ru>
2022-05-02 15:34:24 +0500

    ref: add UpLayerProvider

--- Telegram.Server/ClientApp/src/App.tsx
+++ Telegram.Server/ClientApp/src/App.tsx
@@ -1,64 +1,23 @@
-import React, {ReactElement, useState} from 'react';
+import React from 'react';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import {routes} from "./routes";
 import {AuthenticatedHandler} from "./components/handlers/AuthenticatedHandler";
-import {UpLayer} from "./components/UpLayer";
-import {LeftMenu} from "./components/menus/left-menu";
-import {UpLayerContext} from "./contexts/UpLayerContext";
-import {LeftMenuContext} from "./contexts/LeftMenuContext";
+import {UpLayerProvider} from "./providers/UpLayerProvider";
 
 const App = () => {
-  const [leftMenuVisible, setLeftMenuVisible] = useState(false)
-  const [centralElements, setCentralElements] = useState<ReactElement[]>(() => [])
-
-  const hideUpLayer = () => {
-    setLeftMenuVisible(false)
-    setCentralElements([])
-  }
-  
-  const addCentralElement = (newElement: ReactElement) => {
-    setCentralElements([...centralElements, newElement])
-    setLeftMenuVisible(false)
-    
-    return () => {
-      setCentralElements(elements => {
-        const result = [...elements];
-        const elementIndex = elements.indexOf(newElement)
-        
-        if (elementIndex >= 0) {
-          result.splice(elementIndex, 1)
-        }
-        
-        return result;
-      })
-    }
-  }
-  const removeLastCentralElement = () => {
-    const result = [...centralElements]
-    result.splice(result.length - 1, 1)
-    setCentralElements(result)
-  }
-  
   return (
     <div className="App">
-      <UpLayerContext.Provider value={{addCentralElement, hide: hideUpLayer}}>
-        <LeftMenuContext.Provider value={{setVisible: setLeftMenuVisible}}>
-          <UpLayer
-            leftElementVisible={leftMenuVisible}
-            leftElement={<LeftMenu visible={leftMenuVisible}/>}
-            centerElements={centralElements}
-            onClick={removeLastCentralElement}/>
-          <BrowserRouter>
-            <AuthenticatedHandler>
-              <Switch>
-                {routes}
-              </Switch>
-            </AuthenticatedHandler>
-          </BrowserRouter>
-        </LeftMenuContext.Provider>
-      </UpLayerContext.Provider>
+      <UpLayerProvider>
+        <BrowserRouter>
+          <AuthenticatedHandler>
+            <Switch>
+              {routes}
+            </Switch>
+          </AuthenticatedHandler>
+        </BrowserRouter>
+      </UpLayerProvider>
     </div>
   );
 }
---
+++ Telegram.Server/ClientApp/src/providers/UpLayerProvider.tsx
@@ -0,0 +1,54 @@
+import React, {FC, ReactElement, useState} from "react";
+import {UpLayerContext} from "../contexts/UpLayerContext";
+import {LeftMenuContext} from "../contexts/LeftMenuContext";
+import {UpLayer} from "../components/UpLayer";
+import {LeftMenu} from "../components/menus/left-menu";
+
+export const UpLayerProvider: FC = ({children}) => {
+  const [leftMenuVisible, setLeftMenuVisible] = useState(false)
+  const [centralElements, setCentralElements] = useState<ReactElement[]>(() => [])
+
+  const hideUpLayer = () => {
+    setLeftMenuVisible(false)
+    setCentralElements([])
+  }
+
+  const addCentralElement = (newElement: ReactElement) => {
+    setCentralElements([...centralElements, newElement])
+    setLeftMenuVisible(false)
+
+    return () => {
+      setCentralElements(elements => {
+        const result = [...elements];
+        const elementIndex = elements.indexOf(newElement)
+
+        if (elementIndex >= 0) {
+          result.splice(elementIndex, 1)
+        }
+
+        return result;
+      })
+    }
+  }
+  const removeLastCentralElement = () => {
+    setCentralElements(elements => {
+      const result = [...elements]
+      result.splice(result.length - 1, 1)
+      
+      return result
+    })
+  }
+  
+  return (
+    <UpLayerContext.Provider value={{addCentralElement, hide: hideUpLayer}}>
+      <LeftMenuContext.Provider value={{setVisible: setLeftMenuVisible}}>
+        <UpLayer
+          leftElementVisible={leftMenuVisible}
+          leftElement={<LeftMenu visible={leftMenuVisible}/>}
+          centerElements={centralElements}
+          onClick={removeLastCentralElement}/>
+        {children}
+      </LeftMenuContext.Provider>
+    </UpLayerContext.Provider>
+  )
+}
