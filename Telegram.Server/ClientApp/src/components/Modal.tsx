cf9205c3ac9c15d4778d6b645978876aa0c39664
matcur <ru.far2014@yandex.ru>
2022-07-20 21:46:09 +0500

    new: add useful modals

---
+++ Telegram.Server/ClientApp/src/components/Modal.tsx
@@ -0,0 +1,19 @@
+﻿import {FC, useContext, useEffect} from "react";
+import {ModalsContext} from "./Modals";
+
+type Props = {
+  key: string
+}
+
+export const Modal: FC<Props> = ({children, key}) => {
+  const modalsContext = useContext(ModalsContext)
+  
+  useEffect(() => {
+    modalsContext.insert(children, key)
+  }, [children])
+  useEffect(() => {
+    return () => modalsContext.remove(key)
+  }, [])
+  
+  return null
+}
---
+++ Telegram.Server/ClientApp/src/components/Modals.tsx
@@ -0,0 +1,37 @@
+﻿import {createContext, FC, ReactNode, useCallback, useState} from "react";
+import {classNames} from "../utils/classNames";
+
+type Props = {}
+
+export const ModalsContext = createContext({
+  insert(element: ReactNode, key: string) {},
+  remove(key: string) {},
+})
+
+export const Modals: FC<Props> = ({children}) => {
+  const [modals, setModals] = useState<Record<string, ReactNode | undefined>>({})
+
+  const insert = (element: ReactNode, key: string) => {
+    setModals(modals => ({
+      ...modals,
+      [key]: element,
+    }))
+  }
+  const remove = useCallback((key: string) => {
+    setModals(modals => ({
+      ...modals,
+      [key]: undefined,
+    }))
+  }, [])
+  
+  return (
+    <ModalsContext.Provider
+      value={{insert, remove}}
+    >
+      {children}
+      <div className={classNames("modals", Object.keys(modals).length && "opened-modals")}>
+        {Object.values(modals)}
+      </div>
+    </ModalsContext.Provider>
+  )
+}
