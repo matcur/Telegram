1e0965ee75393fd41e29b75d9ce13bb1db016297
farhullin.rn <farhullin.rn@greendatasoft.ru>
2022-09-23 12:11:52 +0500

    new: add onWindow functions

---
+++ Telegram.Server/ClientApp/src/utils/onWindow.ts
@@ -0,0 +1,30 @@
+﻿import {Nothing} from "./functions";
+import {addIfNotExists} from "./array/addInNotExists";
+import {unsubscribe} from "./array/unsubscribe";
+import {callWith} from "./array/callWith";
+
+const focus: Nothing[] = []
+const blur: Nothing[] = []
+
+const onWindowFocus = (callback: Nothing) => {
+  addIfNotExists(focus, callback)
+  return unsubscribe(focus, callback)
+}
+
+const onWindowBlur = (callback: Nothing) => {
+  addIfNotExists(blur, callback)
+  return unsubscribe(blur, callback)
+}
+
+window.onfocus = () => {
+  callWith(focus, undefined)
+}
+
+window.onblur = () => {
+  callWith(blur, undefined)
+}
+
+export {
+  onWindowFocus,
+  onWindowBlur,
+}
